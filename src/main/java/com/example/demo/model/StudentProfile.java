e41fec5e014eb940845573454321ee322655eb5d
bala-amypo <bala@amypo.in>
2025-12-27 05:28:43 +0000

    User commit

--- src/main/java/com/example/demo/model/StudentProfile.java
+++ src/main/java/com/example/demo/model/StudentProfile.java
@@ -3,7 +3,7 @@ package com.example.demo.model;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public class Student {
+public class StudentProfile {
 
     @NotBlank
     private String studentId;
